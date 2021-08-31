252e1af2b84ecee434850213c04c029aa999971d
00sukhjeet00 <sukhjeet20002@gmail.com>
2021-08-31 20:23:17 +0530

    new commit

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const StudentAuthRoute = require('./Route/studentAuthRoute')
 const teacherRoute = require('./Route/teacherRoute')
 const studentRoute=require('./Route/studentRoute')
 app.use(express.json())
+app.use(express.static('client/build'))
 mongoose.connect(process.env.MONGODB_URI)
     .then(res=>{console.log('DB Connected')})
 app.use('/api/student/',StudentAuthRoute)
@@ -15,7 +16,10 @@ app.use('/api/teacher/', teacherRoute)
 app.use('/api/student/', studentRoute)
 if (process.env.NODE_ENV === 'production')
 {
-    app.use(express.static('client/build'))    
+    app.use(express.static(path.join(__dirname, "client/build")))
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "client/build", "index.html"))
+    })
 }
 const port=process.env.PORT || 5000
 app.listen(port, () => { console.log('Server Started...') })
